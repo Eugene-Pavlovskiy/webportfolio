1b05b1a4b8d83afa965065d6604722cfc1fcce2e
Eugene Pavlovskiy <infopavlovskiy@gmail.com>
2022-07-27 22:06:38 +0200

    added swiper

---
+++ slider.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
